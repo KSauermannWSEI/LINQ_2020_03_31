9eeb381cb586d240b8d7048dd07bbdc0155b47f5
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2020-03-30 20:22:23 +0200

    Add project files.

---
+++ LINQ_2020_03_31/Models/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_2020_03_31.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ LINQ_2020_03_31/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_2020_03_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LINQ World!");
+            Console.WriteLine();
+        }
+
+        #region helpers
+        static void display<T>(List<T> list)
+        {
+            list.ForEach(obj =>
+            {
+                if (obj is String || obj is ValueType)
+                    Console.Write($"{obj} ");
+                else
+                    obj.GetType().GetProperties().ToList().
+                        ForEach(prop => Console.Write($"[{prop.Name}]: {prop.GetValue(obj)} "));
+                Console.WriteLine();
+            });
+        }
+        #endregion
+    }
+}
